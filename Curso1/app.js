8d6a33452e59d7f557c951cffcc3f69ef5b7a3dd
Gabi <gabrielle.sote@gmail.com>
2024-09-15 11:28:07 -0300

    fazendo um site simples

---
+++ Curso1/app.js
@@ -0,0 +1,18 @@
+'use scrict'
+
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function(){
+  document.body.classList.toggle('dark-theme')
+
+  var className = document.body.className;
+  if(className == "light-theme"
+  ){
+    this.textContent = "Dark";
+    console.log('current class name: ' + className);
+  }else{
+    this.textContent = "Light"
+    console.log('current class name: ' + className);
+  }
+})
+ 
+ 
